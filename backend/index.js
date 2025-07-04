1973fc105cdb94ffee6d9f2083b7fd87657d0240
Manan Prjapati <mananprajapati1510@gmail.com>
2025-07-04 11:27:28 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: "https://realtime-mern-chatapp.vercel.app",
+    credentials: true
+}));
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
