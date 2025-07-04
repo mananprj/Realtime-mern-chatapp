ba179ca3f0d52aa630ea014232da5c893fde5d2d
Manan Prjapati <mananprajapati1510@gmail.com>
2025-07-04 10:59:19 +0530

    frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client";
 
-const BaseURL = "http://localhost:5001";
+const BaseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const useAuthStore = create((set,get) => ({
     authUser: null,
