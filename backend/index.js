2c37f0c4564b6165b6d803fd1ac797eb6cccfdf3
Manan Prjapati <mananprajapati1510@gmail.com>
2025-07-04 11:23:11 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+app.use(cors());
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
