d84858d77a3dcb2ee449dda5681674dc21d9a85f
Manan Prajapati <mananprajapati1510@gmail.com>
2025-07-05 11:27:13 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://localhost:5173",
+    origin: "https://realtime-mern-chatapp-frontend.onrender.com",
     credentials: true
 }));
 
